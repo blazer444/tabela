48efd246156465ca0b66491b54f4e19f2f4cb2cc
Cauã Carrijo Gomes Silva <kg364141@gmail.com>
2025-09-30 01:35:54 -0300

    Cadastro DSM1

---
+++ script/script.js
@@ -0,0 +1,12 @@
+console.log("Inicializando Script 01")
+
+function ler() {
+    console.log("Executando função ler...")
+
+}
+
+ console.log("Fechando Execução do Script 02")
+
+ function salvar() {
+    alert("Gravando Dados!!")
+ }
